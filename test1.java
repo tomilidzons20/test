02fba4665b58c6299d7de18a73e5862d3080671c
tomilidzons20 <73526217+tomilidzons20@users.noreply.github.com>
2021-11-04 21:17:06 +0100

    Update test1.java

--- test1.java
+++ test1.java
@@ -79,14 +79,18 @@ public class test3 {
         for(int i = 0; i < arr.length; i++){
             if(count != 1 && arr[i] == c){
                 arr[i] = 0;
-                //arr[i] = ' ';
             }
             if(count == 1 && arr[i] == c){
                 count++;
             }
         }
         String str2 = new String(arr);
-        // str2 = str2.replace(" ", "");
+        for(int i = 0; i < str2.length(); i++){
+            if(str2.charAt(i) == '\0'){
+                str2 = str2.substring(0, i) + str2.substring(i+1);
+            }
+        }
+        System.out.println(str2.length());
         return str2;
     }
     public static void main(String[] args){
@@ -99,4 +103,5 @@ public class test3 {
         in.close();
     }
 }
+
 
